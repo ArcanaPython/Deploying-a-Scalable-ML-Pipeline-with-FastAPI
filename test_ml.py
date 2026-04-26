3afb39c138dc3a4fcae2f7d66b37b9bbde39aec7
Taylor <pythontaylortest@gmail.com>
2026-04-26 14:47:04 -0700

    test_ml.py completed, tests passed, screenshot uploaded

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,52 @@
 import pytest
 # TODO: add necessary import
+import pandas as pd
+from ml.model import train_model, compute_model_metrics
+from ml.data import apply_label
+from sklearn.linear_model import LogisticRegression
+
+
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_model_type():
     """
-    # add description for the first test
+    Tests the model is a LogisticRegression sklearn model.
     """
     # Your code here
-    pass
+    X = pd.DataFrame({
+        "col1": [1, 2, 3, 4, 5],
+        "col2": [1, 1, 0, 0, 1]
+    })
+    y = [1, 1, 1, 0, 0]
+
+    model = train_model(X, y)
+
+    assert isinstance(model, LogisticRegression)
+    
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_metric_types():
     """
-    # add description for the second test
+    Tests that compute_model_metrics returns float datatypes.
     """
     # Your code here
-    pass
+    y_true = [1, 1, 0, 1, 0]
+    y_preds = [0, 1, 1, 0, 0]
+
+    precision, recall, f1 = compute_model_metrics(y_true, y_preds)
+
+    assert isinstance(precision, float)
+    assert isinstance(recall, float)
+    assert isinstance(f1, float)
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_inference_labels():
     """
-    # add description for the third test
+    Tests the string output of the inference label is correct from the 
+    numerical input value.
     """
     # Your code here
-    pass
+    assert apply_label([1]) == ">50K"
+    assert apply_label([0]) == "<=50K"
